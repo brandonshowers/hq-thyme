0c0fe86cec06105cd8fe7047129f092548be6882
Brandon Showers <brandon@workwithopal.com>
2021-09-10 19:19:22 -0700

    Very basic app layout. Add Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+import AppSidebar from "./components/AppSidebar";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <React.Fragment>
+      <AppSidebar className="d-flex flex-column p-3 bg-dark" />
+      <div className="p-3">
+        {/* Children are going to go here */}
+      </div>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/AppSidebar/index.js
@@ -0,0 +1,10 @@
+
+const AppSidebar = ({ className }) => {
+  return (
+    <div className={ className } style={{ width: "280px" }}>
+      <a className="text-white text-decoration-none p-0">hq:thyme</a>
+    </div>
+  )
+}
+
+export default AppSidebar
