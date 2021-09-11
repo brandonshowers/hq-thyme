de2a4f6af01fb417c7d944cc6e7a9e825fbb5cc6
Brandon Showers <brandon@workwithopal.com>
2021-09-10 19:48:18 -0700

    Add react-router-dom and introduce basic routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
 import AppSidebar from "./components/AppSidebar";
 
 function App() {
   return (
-    <React.Fragment>
-      <AppSidebar className="d-flex flex-column p-3 bg-dark" />
+    <Router>
+      <AppSidebar className="d-flex flex-column p-3 bg-dark text-white" />
       <div className="p-3">
-        {/* Children are going to go here */}
+        <Switch>
+          <Route path="/job-offers">
+            <h2>Job Offers</h2>
+          </Route>
+          {/* <Router path="/settings" component={ AppSettings } /> */}
+          <Route path="/">
+            <h2>Dashboard</h2>
+          </Route>
+          {/* <Router path="*" component={ Error404 } /> */}
+        </Switch>
       </div>
-    </React.Fragment>
+    </Router>
   );
 }
 
--- src/components/AppSidebar/index.js
+++ src/components/AppSidebar/index.js
@@ -1,8 +1,21 @@
+import { Link, NavLink } from "react-router-dom"
 
 const AppSidebar = ({ className }) => {
   return (
     <div className={ className } style={{ width: "280px" }}>
-      <a className="text-white text-decoration-none p-0">hq:thyme</a>
+      <Link to="/" className="text-white text-decoration-none p-0 fs-4">hq:thyme</Link>
+      <hr />
+      <ul className="nav nav-pills flex-column mb-auto">
+        <li className="nav-item">
+          <NavLink exact to="/" className="nav-link">Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/job-offers" className="nav-link">Job Offers</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/settings" className="nav-link">Settings</NavLink>
+        </li>
+      </ul>
     </div>
   )
 }
