ffb98889084db6d18f460b74ffec8604845c2d49
Brandon Showers <brandon@workwithopal.com>
2021-09-14 13:53:15 -0700

    Move database seeding to app home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Route,
   Switch
 } from "react-router-dom";
 import AppAdmin from "./components/AppAdmin";
 import Main from "./components/Main";
+import jobOffersJSON from './database/job-offers.json'
 
-function App() {
+const App = () => {
+
+  useEffect(() => {
+    if (!localStorage.getItem("jobOffers")) {
+      const jobs = jobOffersJSON.jobOffers;
+      localStorage.setItem("jobOffers", JSON.stringify(jobs));
+    }
+  }, []);
+
   return (
     <Switch>
       <Route path="/admin" component={ AppAdmin } />
